5bc2b67ec12d9ab4e47cd5c93ac3f4b31cae84dc
Andre Gama del Otero <andredelotero@gmail.com>
2023-04-28 08:59:01 -0300

    fix in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://www.andredelotero.dev/",
