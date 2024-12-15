5b553977264566cc37d23e5732b544917d1a09f2
Andre Gama Del Otero <andredelotero@gmail.com>
2024-12-15 19:38:08 -0300

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://www.andredelotero.dev/",
+  site: "https://andredelotero.dev/",
+  output: "server",
   integrations: [
     tailwind(),
     image({
