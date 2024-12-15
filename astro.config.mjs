b0c71b76bb59506a7d4efb629e2a93ca1163649e
Andre Gama Del Otero <andredelotero@gmail.com>
2024-12-15 19:43:06 -0300

    add vercel adapter to enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: "https://andredelotero.dev/",
-  output: "server",
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     tailwind(),
     image({
